8a4ec7bb14bce68a51b9abc3a9ddfde22f3f8b00
Zach Newby <zpnewby@outlook.com>
2026-09-12 17:07:42 -0500

    Fixed error in organizations.js

--- src/models/organizations.js
+++ src/models/organizations.js
@@ -2,8 +2,8 @@ import db from './db.js'
 
 const getAllOrganizations = async() => {
     const query = `
-        SELECT organization_id, name, description, contact_email, logo_filename
-      FROM public.organization;
+        SELECT id, name, description, contact_email, logo_filename
+      FROM public.organizations;
     `;
 
     const result = await db.query(query);
